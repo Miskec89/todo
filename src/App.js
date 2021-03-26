a6b4d63a3ed2535383c60ab07b30db9e1d66a3c8
Miskec89 <81381186+Miskec89@users.noreply.github.com>
2021-03-26 01:07:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState} from "react"
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {HashRouter as Router, Route} from "react-router-dom"
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import Tasks from "./Components/Tasks"
