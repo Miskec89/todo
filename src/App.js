5bf011e6a7b04ae801ab29d4b8d7a89d073f16b7
Miskec89 <milosmetlic89@gmail.com>
2021-03-26 18:21:03 +0100

    added new logic and style for Fa buttons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./Components/Footer"
 import Tasks from "./Components/Tasks"
 import AddTask from "./Components/AddTask"
 import About from "./Components/About"
-import { FaMoon, FaSun } from "react-icons/fa"
+import { FaMoon, FaSun, FaLongArrowAltRight } from "react-icons/fa"
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false) 
@@ -37,12 +37,22 @@ const toggleReminder = (id) => {
   setTasks(tasks.map((task) => task.id === id? {...task, reminder: !task.reminder} : task ))
 }
 
+const sunColor = {
+  color: '#DBE2E9',
+}
+
+const moonColor = {
+  color: '#8c92ac'
+}
+
   return (
     <Router>
       <div style={{backgroundColor : toggle? "rgba(3, 31, 48, 0.5)" : "#fff"}} className="container">
-        {toggle ? <FaSun onClick={toggler}/> : <FaMoon onClick={toggler}/>}
+        {toggle ?  
+          (<><FaMoon /> <FaLongArrowAltRight /> <FaSun style={sunColor} onClick={toggler}/></>) : 
+          (<><FaSun/> <FaLongArrowAltRight /> <FaMoon style={moonColor} onClick={toggler}/></>) 
+        }
         <Header  onAdd={() => setShowAddTask(!showAddTask)} showAddStatus={showAddTask} />
-        
         <Route 
           path = '/' 
           exact 
